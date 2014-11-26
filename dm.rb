de8ab40aa4dc3fd1fc8ff88172a1a18187f53217
Mike Stolbov <mstolbov@gmail.com>
2014-11-27 01:29:40 +0300

    a little bit refactory simple page loader

--- dm.rb
+++ dm.rb
@@ -1,9 +1,33 @@
 require 'net/http'
 require 'uri'
 
-uri = URI("https://www.google.ru/search?q=funbox&newwindow=1&espv=2&biw=1177&bih=682&source=lnms&tbm=isch&sa=X&ei=J0t2VN-gF8mGywPwzYGYAQ&ved=0CAYQ_AUoAQ")
-h = Net::HTTP.new(uri.host, uri.port)
-h.use_ssl = true if uri.scheme.eql?("https")
-resp = h.get("#{uri.path}?#{uri.query}")
-puts resp.code
-puts resp.message
+class DM
+
+  attr_reader :uri
+
+  class << self
+    def process(url, options = {})
+      new(url, options).perform
+    end
+  end
+
+  def initialize(url, options)
+    #FIXME: Validate url! Should have scheme!
+    @uri = URI.parse url
+    @options = {
+      download_path: "/dev/null",
+      stack_size: 1
+    }.merge(optinos)
+  end
+
+  def perform
+    h = Net::HTTP.new(uri.host, uri.port)
+    h.use_ssl = true if uri.scheme.eql?("https")
+    resp = h.get("#{uri.path}?#{uri.query}")
+    puts resp.code
+    puts resp.message
+  end
+end
+
+# FIXME: move to tests
+DM.process "https://www.google.ru/search?q=funbox&newwindow=1&espv=2&biw=1177&bih=682&source=lnms&tbm=isch&sa=X&ei=J0t2VN-gF8mGywPwzYGYAQ&ved=0CAYQ_AUoAQ"
